aff3a242a252fc425ba281ddaf2d472300b39140
Cody De Arkland <codydearkland@gmail.com>
2022-01-12 12:09:32 -0800

    initial commit

---
+++ main.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+require 'ldclient-rb'
+
+user = {firstName: "Cody", lastName: "De", key: "prod1234", email: "cd@launchdarkly.com"}
+
+get '/' do
+  client = LaunchDarkly::LDClient.new(ENV['LD_SDK_KEY'])
+  pageTheme = client.variation("pageTheme", user, false)
+  @osmoImage = client.variation("osmoImage", user, false)
+  @cards = client.variation("cards", user, false)
+  @imageConfig = client.variation("imageConfig", user, 'toggleUp.png')
+  if pageTheme
+    erb :index
+  else
+    '<h1>Welcome to a sad and unformatted Ruby and LaunchDarkly page</h1>'
+  end
+end
